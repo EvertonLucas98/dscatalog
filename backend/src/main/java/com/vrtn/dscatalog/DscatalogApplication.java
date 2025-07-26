7f4ca4b6fe1c6c5adfe61a6dc9a03ba5d9cdee8f
Everton Lucas <everton.santos@dcomp.ufs.com>
2025-07-26 00:19:18 -0300

    Project created

---
+++ backend/src/main/java/com/vrtn/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.vrtn.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
